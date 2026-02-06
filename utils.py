7509f2827447da658665bb7cb3092b67111be44e
Karthik-410 <karthikorton10@gmail.com>
2026-02-07 00:34:29 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -1,16 +1,8 @@
-import PyPDF2
-import docx
+import spacy
+from spacy.cli import download
 
-def read_txt(file):
-    return file.read().decode("utf-8")
-
-def read_pdf(file):
-    reader = PyPDF2.PdfReader(file)
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text()
-    return text
-
-def read_docx(file):
-    doc = docx.Document(file)
-    return "\n".join(p.text for p in doc.paragraphs)
+try:
+    nlp = spacy.load("en_core_web_sm")
+except:
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
